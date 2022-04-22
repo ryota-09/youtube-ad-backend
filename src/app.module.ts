e8815ebf1435c43c99a0294b518b5202728f8b14
ryota-09 <n.ryo950911@gmail.com>
2022-04-22 13:59:22 +0900

    domain実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { YoutubeModule } from './youtube/youtube.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), YoutubeModule],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), YoutubeModule],
 })
 export class AppModule {}
