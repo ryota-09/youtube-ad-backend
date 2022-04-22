a329dc6ad511a6a2ec784b86276161f2c760a1d0
ryota-09 <n.ryo950911@gmail.com>
2022-04-22 14:45:30 +0900

    supabase連携

--- src/youtube/schedule/schedule.service.ts
+++ src/youtube/schedule/schedule.service.ts
@@ -3,14 +3,14 @@ import { Cron } from '@nestjs/schedule';
 import { DomainService } from '../domain/domain.service';
 
 // 冒頭で広告が流れる動画IDをセットする
-const SOURCE_VIDEO_ID = '3wuw6dGsDrc';
+const SOURCE_VIDEO_ID = 'pvjcT57PIaU';
 
 @Injectable()
 export class ScheduleService {
   private readonly logger = new Logger(ScheduleService.name);
   constructor(private readonly domainService: DomainService) {}
 
-  @Cron('*/10 * * * * *')
+  @Cron('*/5 * * * * *')
   async scheduleForDomainOfFetchAndSaveYouTubeAdsData() {
     this.logger.debug('Started "fetchAndSaveYouTubeAdsData"');
     await this.domainService.fetchAndSaveYouTubeAdsData(SOURCE_VIDEO_ID);
